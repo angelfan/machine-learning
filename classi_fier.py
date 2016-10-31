663401a73a2fec50ce61d95bd627221193bf02aa
angelfan <angelfan1314@163.com>
2016-10-31 23:38:56 +0800

    add(文档过滤):  费舍尔方法

--- classi_fier.py
+++ classi_fier.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import re
+import math
 
 
 def get_words(doc):
@@ -105,6 +106,57 @@ class NavieBayes(Classifier):
         return best
 
 
+class Fisher(Classifier):
+    def __init__(self, get_features):
+        Classifier.__init__(self, get_features)
+        self.minimums = {}
+
+    def cprob(self, f, cat):
+        # 特征在该分类中出现的概率
+        clf = self.fprob(f, cat)
+        if clf == 0: return 0
+
+        # 特征在所有分类中出现的概率
+        freq_sum = sum([self.fprob(f, c) for c in self.categories()])
+
+        return clf / freq_sum
+
+    def prob(self, item, cat):
+        p = 1
+        features = self.get_features(item)
+        for f in features:
+            p *= self.weighted_prob(f, cat, self.cprob)
+
+        fscore = -2 * math.log(p)
+        return self.invchi2(fscore, len(features) * 2)
+
+    def invchi2(self, chi, df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1, df / 2):
+            term *= m / i
+            sum += term
+        return min(sum, 1.0)
+
+    def set_min(self, cat, min):
+        self.minimums[cat] = min
+
+    def get_min(self, cat):
+        if cat not in self.minimums: return 0
+        return self.minimums[cat]
+
+    def classify(self, item, default=None):
+        best = default
+        max = 0.0
+        for c in self.categories():
+            p = self.prob(item, c)
+            if p > self.get_min(c) and p > max:
+                best = c
+                max = p
+
+        return best
+
+
 def sample_train(cl):
     cl.train('Nobody owns the water.', 'good')
     cl.train('the quick rabbit jumps fences', 'good')
@@ -117,3 +169,13 @@ bys = NavieBayes(get_words)
 sample_train(bys)
 print bys.classify('quick rabbit')
 print bys.classify('quick money')
+
+fisher = Fisher(get_words)
+sample_train(fisher)
+print fisher.cprob('quick', 'good')
+print fisher.weighted_prob('money', 'bad', fisher.cprob)
+print fisher.prob('quick rabbit', 'good')
+fisher.set_min('bad', 0.8)
+print fisher.classify('quick money')
+fisher.set_min('bad', 0.4)
+print fisher.classify('quick money')
